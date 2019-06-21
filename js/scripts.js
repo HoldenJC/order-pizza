e53e898f4cba5a304908f6f97e89f3bb59703641
HoldenJC <holdenj_c@yahoo.com>
2019-06-21 11:11:43 -0700

    add prototype function to calculate price of user selected pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 function Pizza(){
-  this.size = "",
+  this.size = 0,
   this.toppings = [],
-  this.price = 0;
+  this.price = 6;
 }
 
 var userPizza = new Pizza();
+
+Pizza.prototype.calculatePrice = function(){
+  this.price = this.price + this.size + (this.toppings.length * 2);
+}
 
 $(function(){
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
 
-    var userSize = $("#selectSize").val();
+    var userSize = parseInt($("#selectSize").val());
 
     var userToppings = [];
     $.each($("input[name='topping']:checked"), function(){
@@ -19,6 +23,8 @@ $(function(){
 
     userPizza.size = userSize;
     userPizza.toppings = userToppings;
+
+    userPizza.calculatePrice();
 
   });
 
