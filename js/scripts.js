2bf5006459d8823ee718b241c45c826b87c48575
HoldenJC <holdenj_c@yahoo.com>
2019-06-21 10:22:43 -0700

    add basic pizza object and store user form input in object

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-function Pizza(size, toppings){
-  this.size = size,
-  this.toppings = toppings,
+function Pizza(){
+  this.size = "",
+  this.toppings = [],
   this.price = 0;
 }
 
-
+var userPizza = new Pizza();
 
 $(function(){
+  $("#pizzaForm").submit(function(event) {
+    event.preventDefault();
 
+    var userSize = $("#selectSize").val();
+
+    var userToppings = [];
+    $.each($("input[name='topping']:checked"), function(){
+      userToppings.push($(this).val());
+    });
+
+    userPizza.size = userSize;
+    userPizza.toppings = userToppings;
+
+  });
 
 });
