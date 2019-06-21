2979cc21d3164ba2e3c2250e4670acf7d80854b6
HoldenJC <holdenj_c@yahoo.com>
2019-06-21 13:33:41 -0700

    add delays to several form transition effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,75 @@
-function Order(){
-  this.pizzas = []
+function Order() {
+  this.pizzas = [],
+    this.total = 0
 }
 
 var order = new Order();
 
-Order.prototype.addPizza = function(pizza){
+Order.prototype.addPizza = function(pizza) {
   this.pizzas.push(pizza);
 }
 
-function Pizza(){
-  this.size = 0,
-  this.toppings = [],
-  this.price = 6;
+function Pizza() {
+    this.size = 0,
+    this.toppings = [],
+    this.price = 6
 }
 
-Pizza.prototype.calculatePrice = function(){
+Pizza.prototype.calculatePrice = function() {
   this.price = this.price + this.size + (this.toppings.length * 2);
 }
 
-function resetPizzaForm(){
-  $.each($("input[type='checkbox']:checked"), function(){
+Pizza.prototype.calculateTotal = function() {
+  order.total += this.price;
+}
+
+function resetPizzaForm() {
+  $.each($("input[type='checkbox']:checked"), function() {
     $(this).prop('checked', false);
   });
   $("#selectSize").get(0).selectedIndex = 0;
 }
 
-function sizeDisplay(pizza){
-  if(pizza.size === 0){
+function sizeDisplay(pizza) {
+  if (pizza.size === 0) {
     return "Small";
-  } else if (pizza.size === 2){
+  } else if (pizza.size === 2) {
     return "Medium";
-  } else if (pizza.size === 4){
+  } else if (pizza.size === 4) {
     return "Large";
   } else {
     return "Extra Large";
   }
 }
 
-function orderUpdate(){
-  $("#currentOrder").empty();
-  var total = 0;
-  $.each($(order.pizzas), function(){
-    var size = sizeDisplay(this);
-    var tops = this.toppings.join(", ");
-    total += this.price;
-    $("#currentOrder").append(size + " pizza with " + tops + ": $" + this.price.toFixed(2) + "<br>");
-  })
-  $("#totalCost").html("Total: $" + total.toFixed(2));
+function orderUpdate(currentPizza) {
+  var size = sizeDisplay(currentPizza);
+  var tops = currentPizza.toppings.join(", ");
+  currentPizza.calculateTotal();
+
+  $("#currentOrder").append("<div style=\"display: none;\" class=\"orderItem\">" + size + " pizza with " + tops + ": $" + currentPizza.price.toFixed(2) + "</div>");
+  $(".orderItem").slideDown();
+  $("#totalCost").html("Total: $" + order.total.toFixed(2));
+  // $(".orderItem").slideDown();
 }
 
-$(function(){
+$(function() {
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
 
     var userPizza = new Pizza();
 
     userPizza.size = parseInt($("#selectSize").val());
-    $.each($("input[name='topping']:checked"), function(){
+    $.each($("input[name='topping']:checked"), function() {
       userPizza.toppings.push($(this).val());
     });
 
     userPizza.calculatePrice();
     order.addPizza(userPizza);
     resetPizzaForm();
-    orderUpdate();
-    $("#currentOrderLabel").show();
-
+    orderUpdate(userPizza);
+    $("#currentOrderLabel").slideDown(800);
+    $("#currentOrder").delay(800).slideDown(800);
+    $("#totalCost").delay(1600).slideDown(800);
   });
 });
